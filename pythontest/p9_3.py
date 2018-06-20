4c42a4331bb5e58c5ff88fad825519e52f5d33e8
KoukiTazawa <40353211+KoukiTazawa@users.noreply.github.com>
2018-06-20 15:44:40 +0900

    Update p9_3.py

--- pythontest/p9_3.py
+++ pythontest/p9_3.py
@@ -3,6 +3,13 @@ import matplotlib.colors as mcolors
 import numpy as np
 from numpy.random import multivariate_normal
 
+#変更部分
+
+plt.rcParams['font.family'] = 'Times New Roman' #全体のフォントを設定
+plt.rcParams['font.size'] = 20 #フォントサイズを設定
+plt.rcParams['axes.linewidth'] = 1.5 #軸の太さを設定。目盛りは変わらない
+
+
 data = np.vstack([
     multivariate_normal([10, 10], [[3, 2], [2, 3]], size=100000),
     multivariate_normal([30, 20], [[2, 3], [1, 3]], size=1000)
@@ -19,6 +26,7 @@ for ax, gamma in zip(axes.flat[1:], gammas):
     ax.set_title('Power law $(\gamma=%1.1f)$' % gamma)
     ax.hist2d(data[:, 0], data[:, 1],
               bins=100, norm=mcolors.PowerNorm(gamma))
+
 
 fig.tight_layout()
 
